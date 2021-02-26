45f628b4e48d33742b6a3f886e0d4df1ce0d1994
gjsimkins <baseball3gary@gmail.com>
2021-02-25 22:42:33 -0800

    Initial timer page css

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Time in milliseconds
-const SHORT_BREAK_DURATION = 1 * 1000;
+const SHORT_BREAK_DURATION = 10 * 1000;
 const LONG_BREAK_DURATION = 1 * 1000;
-const WORK_DURATION = 1 * 1000;
+const WORK_DURATION = 10 * 1000;
 const UPDATE_TIMER_EVERY = 200;
 
 const LONG_BREAK_EVERY = 4;
