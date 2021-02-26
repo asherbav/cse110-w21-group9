13e333821ab3d970313cc15c4c0ca6b90e0d994b
Pootisplank <ialelin3401@gmail.com>
2021-02-25 17:42:46 -0800

    created jest tests for data storage functions

--- index.test.js
+++ index.test.js
@@ -1,25 +1,52 @@
-// createPomodoro, logDistraction, setName, setStatus, getPomo, getPomoById, getCurrentPomoId, setPomo
-
+/* 
+createPomodoro, X
+logDistraction, X
+setName, X
+setStatus, X
+getPomo, X
+getCurrentPomoId, 
+setPomo 
+*/
+const index = require("./index.js");
 jest.useFakeTimers();
 
 describe("data storage tests", () => {
   test("create pomodoro test", () => {
     index.createPomodoro("test task", 4);
-    expect(JSON.stringify(pomoData[i])).toBe(
+    expect(JSON.stringify(index.pomoData[0])).toBe(
       JSON.stringify({
         id: 0,
         taskName: "test task",
         estimatedPomos: 4,
         actualPomos: 0,
         distractions: 0,
-        sessionStatus: SESSION_STATUS.incomplete,
+        sessionStatus: index.SESSION_STATUS.incomplete,
       })
     );
   });
 
   test("log distraction test", () => {
     index.logDistraction(0);
-    expect(pomoData[0].distractions).toBe(1);
+    expect(index.pomoData[0].distractions).toBe(1);
+  });
+
+  test("set pomo status test", () => {
+    index.setStatus(0, 1);
+    expect(index.pomoData[0].sessionStatus).toBe(1);
+  });
+
+  test("set pomo name test", () => {
+    index.setName(0, "updated test task");
+    expect(index.pomoData[0].taskName).toBe("updated test task");
+  });
+
+  test("get pomo test", () => {
+    expect(index.getPomo(0).taskName).toBe("updated test task");
+  });
+
+  test("set/get current pomo test", () => {
+    index.setPomo(-1);
+    expect(index.getCurrentPomoId()).toBe(-1);
   });
 });
 
