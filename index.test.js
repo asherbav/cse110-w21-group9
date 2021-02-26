7dc1f8f8d6a4968f2e925a74897ab7da13d4310e
Pootisplank <ialelin3401@gmail.com>
2021-02-25 17:27:46 -0800

    jest test

--- index.test.js
+++ index.test.js
@@ -1,8 +1,31 @@
-const index = require('./index.js')
+// createPomodoro, logDistraction, setName, setStatus, getPomo, getPomoById, getCurrentPomoId, setPomo
 
 jest.useFakeTimers();
 
-test('date string format test', () => {
-	var date = new Date(1613616935705)
-	expect(index.getTimeString(date)).toBe("55:35");
+describe("data storage tests", () => {
+  test("create pomodoro test", () => {
+    index.createPomodoro("test task", 4);
+    expect(JSON.stringify(pomoData[i])).toBe(
+      JSON.stringify({
+        id: 0,
+        taskName: "test task",
+        estimatedPomos: 4,
+        actualPomos: 0,
+        distractions: 0,
+        sessionStatus: SESSION_STATUS.incomplete,
+      })
+    );
+  });
+
+  test("log distraction test", () => {
+    index.logDistraction(0);
+    expect(pomoData[0].distractions).toBe(1);
+  });
+});
+
+describe("utilities", () => {
+  test("date string format test", () => {
+    var date = new Date(1613616935705);
+    expect(index.getTimeString(date)).toBe("55:35");
+  });
 });
