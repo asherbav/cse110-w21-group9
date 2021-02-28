8481e2979c82003c69e4bf329e99eb534517d9e0
Eswarsai Ramineni <eraminen@ucsd.edu>
2021-02-28 00:13:05 -0800

    Fixed indentations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Time in milliseconds
 const SHORT_BREAK_DURATION = 10 * 1000;
 const LONG_BREAK_DURATION = 1 * 1000;
-const WORK_DURATION = 1 * 1000;
+const WORK_DURATION = 10 * 1000;
 const UPDATE_TIMER_EVERY = 200;
 
 const LONG_BREAK_EVERY = 4;
@@ -117,6 +117,8 @@ function finishPomo() {
 	let timerpage = document.getElementById('timer-page');
 	mainpage.style.display = '';
 	timerpage.style.display = 'none';
+	let panel = document.getElementById("cancel-button-dialog");
+	panel.close();
 	updateTable();
 	if (cancelTimerFlag == 1) {
 		cancelTimerFlag = 0;
